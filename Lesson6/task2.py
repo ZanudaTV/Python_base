62ac3592cdd1bdd7643822e6cbb6e2e4cf45a90b
Teslya Anton <toxa77725@gmail.com>
2023-02-16 23:40:50 +0300

    dz 6 & 6 commit

---
+++ Lesson6/task2.py
@@ -0,0 +1,8 @@
+# Определить индексы элементов массива (списка),
+# значения которых принадлежат заданному диапазону
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+list1 = [7, 9, 17, 13, 15, 4, 3, 54, 23, 3, 2]
+minNum = int(input("enter min number = "))
+maxNum = int(input("enter max number = "))
+
+print(*[i for i in range(len(list1)) if minNum <= list1[i] <= maxNum])
