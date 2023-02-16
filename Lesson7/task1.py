76d49d4b81d46cb578d96fa8dcdb1ecfd79b310d
Teslya Anton <toxa77725@gmail.com>
2023-02-16 23:41:01 +0300

    dz 6 & 6 commit

---
+++ Lesson7/task1.py
@@ -0,0 +1,10 @@
+example_string = "аяуюоеёэиы"
+list_str = "пара-ра-рам рам-пам-папам па-ра-па-дам".split()
+list_len = []
+
+for one_str in list_str:
+    list_vowels = [vowels for vowels in one_str if vowels in example_string]  # [a,a,a,a]
+    # list_vowels = list(filter(lambda x: x in example_string, one_str)) 2й вариант тоже самое что и стр выше
+    list_len.append(len(list_vowels))
+
+print("Парам пам-пам" if len(set(list_len)) == 1 else "Пам парам")
