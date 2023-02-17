ddcc261b5acd8d3fc3baea0a7d85589597144f75
Teslya Anton <toxa77725@gmail.com>
2023-02-17 21:16:47 +0300

    dz 6 & 6 commit

--- Lesson7/task2.py
+++ Lesson7/task2.py
@@ -7,8 +7,13 @@
 #
 # print_operation_table(lambda x, y: x * y)
 
-num_rows = int(input('колличество строк = '))
-num_columns = int(input('колличество столбцов = '))
 
-def table(num_rows, num_columns):
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for i in range(1, num_rows + 1):
+        for j in range(1, num_columns + 1):
+            print(operation(i, j), end='\t')
+        print()
     
+
+
+print_operation_table(lambda x, y: x * y, int(input('колличество строк = ')), int(input('колличество столбцов = ')))
