e374d3db4d7763178b317b74ad49a270b1c515a6
Teslya Anton <toxa77725@gmail.com>
2023-03-04 23:52:04 +0300

    dz 8 & 8 commit

--- Lesson7/task2.py
+++ Lesson7/task2.py
@@ -8,12 +8,28 @@
 # print_operation_table(lambda x, y: x * y)
 
 
-def print_operation_table(operation, num_rows=6, num_columns=6):
-    for i in range(1, num_rows + 1):
-        for j in range(1, num_columns + 1):
-            print(operation(i, j), end='\t')
-        print()
-    
+# def print_operation_table(operation, num_rows=6, num_columns=6):
+#     for i in range(1, num_rows + 1):
+#         for j in range(1, num_columns + 1):
+#             print(operation(i, j), end='\t')
+#         print()
+#
+#     # print([i * j for i in range(1, num_rows + 1) for j in range(1, num_columns + 1)])
+#
+#
+#
+#
+# print_operation_table(lambda x, y: x * y, int(input('колличество строк = ')), int(input('колличество столбцов = ')))
 
-
-print_operation_table(lambda x, y: x * y, int(input('колличество строк = ')), int(input('колличество столбцов = ')))
+numWatermelon = int(input('Enter the number of watermelons: '))
+massWatemelons = [int(input('Enter the mass of watermelons through ')) for value in range(numWatermelon)]
+maxMass = massWatemelons[0]
+minMass = massWatemelons[0]
+print(massWatemelons)
+for i in massWatemelons:
+    if i > maxMass:
+        maxMass = i
+    if i < minMass:
+        minMass = i
+print('Minimum weight =', minMass)
+print('Maximum weight =', maxMass)
